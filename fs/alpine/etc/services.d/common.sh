e812a663fa38c3aad6ed5b3a542730c2e4ef5adf
Matthew Baggett <matthew@baggett.me>
2025-06-10 20:09:28 +0200

    Baseline improvements

--- fs/alpine/etc/services.d/common.sh
+++ fs/alpine/etc/services.d/common.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155
-set -eE -o functrace
+set -e -o functrace
 if [[ ${BASH_SOURCE[0]} == "${0}" ]]; then
 	echo "This script is intended to be sourced, not executed directly."
 	exit 1
 fi
+
+export DIR
+# Some colour codes.
+export COLOR_RESET=$(tput sgr0)
+export COLOR_GREEN=$(tput setaf 2)
+export COLOR_RED=$(tput setaf 9)
+export COLOR_YELLOW=$(tput setaf 3)
+export COLOR_GREY=$(tput setaf 8)
+
+set_dir_name() {
+	local dir_name="${PROCESS_NAME:-$(basename "$(dirname "$(readlink -f "$0")")")}"
+	local color=${PROCESS_COLOR:-$(echo $dir_name | cksum | awk '{print $1%230 + 1}')}
+	DIR="$(tput setaf "$color")$dir_name$(tput sgr0)"
+}
 
 failure() {
 	local lineno=$1
@@ -13,17 +27,7 @@ failure() {
 }
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-export DIRECTORY="$(dirname "$(readlink -f "$0")")"
-export PROCESS_COLOR=$(basename "$DIRECTORY" | cksum | awk '{print $1%230 + 1}')
-DIR=${DIR:-$(basename "$DIRECTORY")}
-export DIR="$(tput setaf "$PROCESS_COLOR")$DIR$(tput sgr0)"
-
-# Some colour codes.
-export COLOR_RESET=$(tput sgr0)
-export COLOR_GREEN=$(tput setaf 2)
-export COLOR_RED=$(tput setaf 9)
-export COLOR_YELLOW=$(tput setaf 3)
-export COLOR_GREY=$(tput setaf 8)
+set_dir_name
 
 # if /app exists, change to it.
 if [ -d /app ]; then
